1907145c6dcdc9cb2d2011cc2df258324a0aa881
Matt Micene <nzwulfin@gmail.com>
2017-01-04 11:53:01 -0500

    Updating structure of platform containers, added new Kaggle experiment

--- R/Dockerfile.R
+++ R/Dockerfile.R
@@ -1,14 +1,16 @@
+# docker build -t rplatform .
 FROM rhel7
 
 # Needs rhel-7-server-optional-rpms enabled on the docker host that builds the container
 RUN yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 
-RUN INSTALL_PKGS="texinfo-tex R" && \
+RUN INSTALL_PKGS="texinfo-tex R ed" && \
+yum-config-manager --disable "*" && \
+yum-config-manager --enable rhel-7-server-rpms && \
 yum-config-manager --enable rhel-7-server-optional-rpms && \
+yum-config-manager --enable epel && \
 yum -y install $INSTALL_PKGS && \
 yum -y clean all
-
-RUN R -e 'install.packages("SuppDists", dependencies = TRUE, repos="http://cran.us.r-project.org")'
 
 # docker run -e RTEST=/tmp/benchmark
 CMD  R < $RTEST --slave
